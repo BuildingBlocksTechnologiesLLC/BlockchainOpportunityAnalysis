145da6788a707261b4a0369c71f1836fcda170a1
XiaoluQianUW <qianx6@uw.edu>
2020-08-06 00:50:43 -0700

    Update ml_classifier.py

--- ml_classifier.py
+++ ml_classifier.py
@@ -1,4 +1,4 @@
-# laod dependencies
+# load dependencies
 import pickle
 import numpy as np
 import pandas as pd
@@ -6,94 +6,92 @@ import os, json
 from sklearn import preprocessing
 import time
 import re
-from sklearn.feature_extraction.text import CountVectorizer
-from collections import Counter
-from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# load the model from disk
+filename = 'svm_classifier.sav'
+loaded_model = pickle.load(open(filename, 'rb'))
+# load the trained dictionary for vocab
+with open('vocab.pickle', 'rb') as handle:
+    vocab = pickle.load(handle)
 
 '''
 Open files and create dataframe with the wanted features
 '''
-def open_file(path, domain):
-    json_files = [pos_json for pos_json in os.listdir(path + domain)]
+def open_file(path):
     df = pd.DataFrame(columns=['title_loc','title_org', 'salary', 'body'])
-    posting = []
-    for index, js in enumerate(json_files):
-        with open(os.path.join(path + domain, js)) as json_file:
-            if js != '.DS_Store':
-                job = json.load(json_file)
+    with open(path ,"r") as json_file:
+        job = json.load(json_file)
 
-                # include NER entities
-                if "CompanyName" in job:
-                    posting.append(js)
-                    title_loc = job['Location']
-                    title_org = job['CompanyName']
-                    salary = job['Salary']
-                    body = job['Body']
-                    df.loc[index] = [title_loc, title_org, salary, body]
-                else:
-                    continue
-    return df, posting
+        # include NER entities
+        if "CompanyName" in job:
+            title_loc = job['Location']
+            title_org = job['CompanyName']
+            salary = job['Salary']
+            body = job['Body']
+            df.loc[0] = [title_loc, title_org, salary, body]
+    return df
 
 
 '''
 preprocess the testing set for model prediction of the classifier
 '''
-def prediction(career_df,loaded_model):
+
+def prediction(career_df):
     career_df = career_df.replace({'title_loc' : 'Not Found', 'title_org' : 'Not Found', 'salary' : 'Not Found'}, 0)
     career_df.loc[~career_df["title_loc"].isin([0]), "title_loc"] = 1
     career_df.loc[~career_df["title_org"].isin([0]), "title_org"] = 1
     career_df.loc[~career_df["salary"].isin([0]), "salary"] = 1
 
-    body_list = career_df['body'].tolist()
-    count_vect = CountVectorizer(max_features=100)
-    x_train_counts = count_vect.fit_transform(body_list)
+    import preprocessing as prep
+    # function to perform lemmatize and stem preprocessing steps on the data set.
+    preprocessor = prep.Preprocessor(career_df['body'])
+    preprocessor.preprocess(lemmatize=True, stopwords=[], min_token_length=3)
+    preprocessor.get_bigrams_from_preprocessed()
+    preprocessed_text = preprocessor.preprocessed_text_
 
-    tfidf_transformer = TfidfTransformer()
-    x_test_tfidf = tfidf_transformer.fit_transform(x_train_counts)
+    body_list = []
+    for i, arr in enumerate(preprocessed_text):
+        l = ' '.join(preprocessed_text[i])
+        body_list.append(l)
+
+    vectorizer2 = TfidfVectorizer(vocabulary= vocab)
+    x_test_tfidf = vectorizer2.fit_transform(body_list)
     x_test_tfidf = x_test_tfidf.toarray()
-
     shape = x_test_tfidf.shape[0]
     salary = np.array(career_df["salary"]).reshape(shape, 1)
-    feature_matrix_1 = np.concatenate([x_test_tfidf, salary], axis=1)
+    feature_matrix_1 = np.hstack((x_test_tfidf, salary))
     title_loc = np.array(career_df["title_loc"]).reshape(shape, 1)
     title_org = np.array(career_df["title_org"]).reshape(shape, 1)
     feature_matrix_2 = np.hstack((title_loc, title_org))
     X_test = np.hstack((feature_matrix_1, feature_matrix_2))
 
-    prediction = loaded_model.predict(X_test)
-    return prediction
+    predict = loaded_model.predict(X_test)
+    return predict
 
 
 '''
 overwrite the same json file and add one entity for the indicator of job_posting
 '''
-def output(path, domain, posting, prediction):
-    for index, js in enumerate(posting):
-        with open(path +domain+'/'+js,"r") as json_file:
-            job = json.load(json_file)
-            new = {}
-            with open(path +domain+'/'+ js, "w") as outfile:
-                if prediction[index] == 1:
-                    new['job_posting'] = 'True'
-                else:
-                    new['job_posting'] = 'False'
-                job.update(new)
-                json_str = json.dumps(job, indent = 4) + "\n"
-                outfile.write(json_str)
+def output(path, predict):
+    with open(path,"r") as json_file:
+        job = json.load(json_file)
+        new = {}
+        with open(path, "w") as outfile:
+            if predict[0] == 1:
+                new['job_posting'] = 'True'
+            else:
+                new['job_posting'] = 'False'
+            job.update(new)
+            json_str = json.dumps(job, indent = 4) + "\n"
+            outfile.write(json_str)
 
 
 if __name__ == "__main__":
-    #load trained model
-    filename = 'svm_classifier.sav'
-    loaded_model = pickle.load(open(filename, 'rb'))
-
-    #file path
-    predict_path = 'Data/Jobs/'
-
-    # domain of the files
-    domain = 'careers'
-    career_df, posting = open_file(predict_path, domain)
+    # file path
+    predict_path = 'Data/Jobs/careers/swazm_BC8818A7EC327C93BC7FFDFA6FE94327_'
+    career_df = open_file(predict_path)
 
     # prediction
-    prediction = prediction(career_df)
-    output(predict_path,domain, posting, prediction)
+    predict = prediction(career_df)
+    output(predict_path, predict)
