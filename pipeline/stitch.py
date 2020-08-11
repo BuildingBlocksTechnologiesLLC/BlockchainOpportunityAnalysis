03d07dc0bae082db61b16d17c700c033c8b59742
Leon Lu <leonjiaolu@gmail.com>
2020-08-11 12:04:31 -0400

    final update

--- pipeline/stitch.py
+++ pipeline/stitch.py
@@ -11,4 +11,5 @@ if __name__ == "__main__":
     os.system('python NLP.py '+file_path+' '+stanfordnlp+' '+stanfordner)
     os.system('python ml_classifier.py ' + file_path+'nlp')
 #C:/Users/Leon/Data/Jobs/dice/0EF0C801B58186F0A53370366E92089E_
-
+#C:/Users/Leon/Data/Jobs/linkedin/C30CBE1458C0E7EA1DBF201D62ECCC33_
+#C:/Users/Leon/Data/Jobs/glassdoor/0B1A36D88FFF51944CA51A4A3B0A1C60_
